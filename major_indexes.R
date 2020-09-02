5a47af336e803d5612c43e1dcec35e569019115f
David Allen <davidallen02@me.com>
2020-09-02 14:21:04 -0700

    Update charts

--- major_indexes.R
+++ major_indexes.R
@@ -1,40 +1,17 @@
 library(magrittr)
 
-headline <- pamngr::get_data("napmnmi")
-production <- pamngr::get_data("napmprod")
-new_orders <- pamngr::get_data("napmnewo")
-deliveries <- pamngr::get_data("napmsupl")
+dat <- pamngr::join_sheets(c("napmnmi","napmprod","napmnewo","napmempl")) %>%
+  magrittr::set_colnames(c(
+    "dates", "Headline", "Production","New Orders", "Employment"
+    )) %>%
+  reshape2::melt(id.vars = "dates") %>%
+  dplyr::slice_max(dates, n = 60) 
+p <- dat %>% 
+  pamngr::lineplot() %>%
+  pamngr::pam_plot(plot_title = "ISM Manufacturing Index")
 
-ism_manuf <- headline %>%
-  dplyr::left_join(production, by = "Dates") %>%
-  dplyr::left_join(new_orders, by = "Dates") %>%
-  dplyr::left_join(deliveries, by = "Dates") %>%
-  set_colnames(c("Dates", "Headline", "Production","New Orders", "Supplier Deliveries")) %>%
-  reshape2::melt(id.vars = "Dates") %>%
-  dplyr::group_by(variable) %>%
-  dplyr::slice_max(Dates, n = 60)
+p <- p + 
+  ggplot2::geom_hline(yintercept = 50, color = "black") +
+  ggplot2::facet_wrap(. ~ variable, nrow = 2) 
 
-p <- ism_manuf %>%
-  ggplot2::ggplot(ggplot2::aes(Dates, value, color = variable)) +
-  ggplot2::geom_line(size = 2) +
-  ggplot2::facet_wrap(. ~ variable, nrow = 2) +
-  ggplot2::scale_color_manual(values = c("#850237", "black","blue","red")) +
-  ggplot2::labs(title = "ISM Manufacturing Index") +
-  ggplot2::theme(
-    plot.title = ggplot2::element_text(face = "bold", size = ggplot2::rel(3.25)),
-    legend.title = ggplot2::element_blank(),
-    legend.text = ggplot2::element_text(size = ggplot2::rel(1.5)),
-    legend.position = "none",
-    axis.title = ggplot2::element_blank(),
-    axis.text = ggplot2::element_text(size = ggplot2::rel(1.5)),
-    strip.text = ggplot2::element_text(size = ggplot2::rel(1.5), color = "white"),
-    strip.background = ggplot2::element_rect(fill = "#850237")
-  )
-
-ggplot2::ggsave(
-  filename = "./output/ppt/major-indexes.png",
-  plot     = p,
-  width    = 13.33,
-  height   = 7.5,
-  units    = 'in'
-)
+p %>% pamngr::all_output("major-indexes")
